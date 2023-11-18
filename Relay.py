f69d62c5a28298d6d6c3bbe01ece525f0ce65a63
michael <mishacottow@gmail.com>
2023-11-18 00:01:30 -0500

    relay code

--- Relay.py
+++ Relay.py
@@ -4,11 +4,18 @@ import RPi.GPIO as GPIO
 class Relay(object):
     def __init__(self, pin):
       self.pin = pin
+      self.state = False
       GPIO.setmode(GPIO.BCM)
       GPIO.setup(pin, GPIO.OUT)
 
     def off(self):
+      if not self.state:
+         return 
       GPIO.output(self.pin, GPIO.LOW) # out
+      self.state = False
 
     def on(self):
+      if self.state:
+         return 
       GPIO.output(self.pin, GPIO.HIGH) # out
+      self.state = True
