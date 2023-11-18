2d506f2307fde84c29788a85b6571a02f19d72b5
michael <mishacottow@gmail.com>
2023-11-17 23:48:38 -0500

    relay code

--- Relay.py
+++ Relay.py
@@ -1,28 +1,7 @@
 import RPi.GPIO as GPIO
-import time
-
-in1 = 23
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.output(in1, False)
-
-try:
-    while True:
-      for x in range(5):
-            GPIO.output(in1, True)
-            time.sleep(5)
-            GPIO.output(in1, False)
-            time.sleep(5)
-      
-      GPIO.output(in1,True)
-
-      for x in range(4):
-            GPIO.output(in1, True)
-            time.sleep(2)
-            GPIO.output(in1, False)
-            time.sleep(2)
-      GPIO.output(in1,True)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+ 
+RELAIS_1_GPIO = 23
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
