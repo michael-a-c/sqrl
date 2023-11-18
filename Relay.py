6b94e26d8ff7c245d91da4f8efe21966e6b255b4
michael <mishacottow@gmail.com>
2023-11-17 23:50:16 -0500

    relay code

--- Relay.py
+++ Relay.py
@@ -1,7 +1,15 @@
+import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
  
 RELAIS_1_GPIO = 23
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+print(1)
+time.sleep(2)
 GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+print(2)
+time.sleep(2)
+GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+print(3)
